1091ddd867fb4b0eba8029af69c41ecf3e97299c
narakushi <taina.santana1305@gmail.com>
2025-04-19 20:48:32 -0300

    :sparkles: feat: adicionando melhorias e limpando o codigo

--- src/exercicios/04_ListaDinamica/ListaDinamica.jsx
+++ src/exercicios/04_ListaDinamica/ListaDinamica.jsx
@@ -8,17 +8,12 @@ const ListaDinamica = () => {
     const novosNomes = [...nomes];
     novosNomes.splice(index, 1);
     setNomes(novosNomes);
-  }
-
-  const limpaInput = () => {
-    setNome("");
   };
+
+  const limpaInput = () => setNome("");
 
   const adicionaNome = (nome) => {
     if (nome != "") {
-      //nomes.push(nome); //modificação direta no state, é correto?
-      // ou é melhor criar um novo array e setar o state por meio do set?
-      // ou adicionar por meio do prevState
       setNomes((prev) => [...prev, nome]);
       limpaInput();
     } else {
@@ -32,17 +27,17 @@ const ListaDinamica = () => {
         type="text"
         value={nome}
         onChange={(e) => setNome(e.target.value)}
+        onKeyDown={(e) => {if(e.key === 'Enter') adicionaNome(e.target.value)}}
       />
       <button onClick={() => adicionaNome(nome)}>Adicionar</button>
 
       <h3>Nomes adicionados</h3>
       <ul>
         {nomes.map((currentItem, index) => (
-          <>
-            <li key={index}>
-              {currentItem} <button onClick={() => removerItem(index)}>Remover</button>
-            </li>
-          </>
+          <li key={index}>
+            {currentItem}{" "}
+            <button onClick={() => removerItem(index)}>Remover</button>
+          </li>
         ))}
       </ul>
     </div>
